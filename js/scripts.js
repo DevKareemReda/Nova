bbbd116ab8fdac12bb63a4052846949b9040329d
kareem reda <147895396+DevKareemReda@users.noreply.github.com>
2024-02-18 12:25:59 -0800

    the project has been completed, added all pages and pre-loader

--- js/scripts.js
+++ js/scripts.js
@@ -29,6 +29,9 @@ $(function () {
   });
 });
 
+let preloader = document.querySelector(".preloader");
+window.onload = () => setTimeout(() => preloader.classList.add("show"), 300);
+
 let openDrop = document.querySelectorAll(".fa-angle-down");
 openDrop.forEach((el) => {
   el.onclick = function (e) {
@@ -51,3 +54,24 @@ closeNav.onclick = function () {
   openNav.classList.remove("active");
 };
 
+let customPagination = document.querySelectorAll(".custom-pagination a");
+let tabsInner = document.querySelectorAll(".tab-blog");
+
+customPagination.forEach((el) => {
+  el.onclick = (e) => {
+    e.preventDefault();
+    
+    // remove class visible when click on pagination
+    customPagination.forEach((el) => el.classList.remove("active"));
+    el.classList.add("active");
+
+    // filter class visible when click on pagination
+    tabsInner.forEach((el) => el.classList.remove("visible"));
+    document
+      .querySelectorAll("." + el.getAttribute("data-filter"))
+      .forEach((el) => el.classList.add("visible"));
+  };
+});
+
+let copy = document.querySelector(".copy span");
+copy.textContent = new Date().getFullYear();
